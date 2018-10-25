12d7ac0f8b022b32c00fcc88bdb4fb201d6c3278
josea2017 <josea3712@gmail.com>
2018-10-24 21:39:05 -0600

    reparando gui de home index

--- shared/menu.php
+++ shared/menu.php
@@ -1,11 +1,11 @@
 <link rel="stylesheet" type="text/css" href="../assets/css/style_menu.css">
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-light" id="nav_principal">
   <a class="navbar-brand" href="#"><?=$tituloPagina?></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <div class="collapse navbar-collapse" id="navbarSupportedContent" style="margin-left: 80%;">
     <ul id="menu_opciones" class="navbar-nav mr-auto">
         <?php
         $menu = [
@@ -13,7 +13,7 @@
           //'Salir' => '../seguridad/login.php',
         ];
 
-            echo "<label class='nombre_usuario'>".$_SESSION['usuario']['nombre']."|</label>";
+            echo "<label id='nombre_usuario' style='margin-top: 5%;''>".$_SESSION['usuario']['nombre']."|</label>";
         foreach ($menu as $key => $value) {
             echo "<li class='nav-item'>
                     <a class='nav-link' href='$value'>$key</a>
