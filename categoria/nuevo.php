e690cc7f2b34b093232c5ba1c37f6465a37ac9d5
josea2017 <josea3712@gmail.com>
2018-10-25 22:15:17 -0600

    creando categorias

---
+++ categoria/nuevo.php
@@ -0,0 +1,46 @@
+<?php 
+$title='Eshop-Crear Categoria';
+$tituloPagina = 'Crear categoría';
+require_once '../shared/header.php';
+require_once '../shared/sessions.php';
+require_once '../shared/menu.php';
+require_once '../seguridad/verificar_session.php';
+require_once '../modelos/Categoria.php';
+
+$id_categoria = $_POST['id_categoria'] ?? '';
+$nombre = $_POST['nombre'] ?? '';
+if($id_categoria != '' && $nombre != ''){
+	$categoria_modelos = new Categoria();
+	if($categoria_modelos->validarRegistro($id_categoria, $nombre)){
+		echo "<script type='text/javascript' href'./registro.php'>alert('Registro exitoso');</script>";
+
+	}else{
+		echo "<script type='text/javascript' href'./nuevo.php'>alert('Datos incompletos y/o Id categoria ya existe');</script>";
+	}
+}
+
+
+ ?>
+ <link rel="stylesheet" type="text/css" href="../assets/css/style_index_categoria.css">
+
+
+<form action="./nuevo.php" method="POST">
+<div class="div_tabla_crear_categoria">
+  <table class="tabla_crear_categoria" cellspacing="0" cellpadding="6">
+    <tr>
+      <td>ID CATEGORIA: <input type="text" name="id_categoria" autofocus placeholder="Id categoria" value="<?= isset($_POST['id_categoria']) ? $_POST['id_categoria'] : ''; ?>"></td>
+    </tr>
+    <tr>
+      <td>NOMBRE: <input type="text" name="nombre" placeholder="Nombre" value="<?= isset($_POST['nombre']) ? $_POST['nombre'] : ''; ?>"></td>
+    </tr>
+    <tr>
+      <td><button class="btn btn-success" type="submit">Guardar</button></td>
+    </tr>
+  </table>
+</div>
+</form>
+
+ <?php 
+ require_once '../shared/footer.php';
+  ?>
+
--- modelos/Categoria.php
+++ modelos/Categoria.php
@@ -14,15 +14,39 @@ class Categoria
         $resultado = $con->runQuery($sql);
         return $resultado; 
     }
-
-    public function insertar($nombre, $apellidos, $telefono, $correo, $direccion, $rol, $usuario, $contrasenna)
+    public function validarRegistro($id_categoria, $nombre)
     {
-        /*
-      $sql = "INSERT INTO usuarios(nombre, apellidos, telefono, correo, direccion, rol, id_usuario, contrasenna) VALUES ('$nombre','$apellidos', '$telefono', '$correo', '$direccion', '$rol', '$usuario', '$contrasenna')";
+      $resultado = false;
+      if($this->validarCodigoDisponible($id_categoria)){
+        $resultado = true;
+        $this->insertar($id_categoria, $nombre);
+      }
+      return $resultado;    
+    }
+    public function validarCodigoDisponible($id_categoria){
+      $resultado = false;
+      $contador = 0;
+
+      $con = new PgConnection('localhost', '5432', 'jose', '12345', 'eshop');
+      $con->connect();
+
+      $resultado_db = null;
+      $resultado_db = $con->runQuery("SELECT * FROM categorias WHERE id_categoria = '$id_categoria'");
+
+      if(empty($resultado_db)){
+        $resultado = true;
+      }
+
+      return $resultado;  
+    }
+    public function insertar($id_categoria, $nombre)
+    {
+        
+      $sql = "INSERT INTO categorias(id_categoria, nombre) VALUES ('$id_categoria','$nombre')";
       $con = new PgConnection('localhost', '5432', 'jose', '12345', 'eshop');
       $con->connect();
       $con->runStatement($sql);
-        */
+        
     }
     
 
