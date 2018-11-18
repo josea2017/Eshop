1fdc7f6b8f4baa842deb2ee541244dae30fdd44f
josea2017 <josea3712@gmail.com>
2018-11-18 15:30:54 -0600

    UI menu de navegacion

--- shared/menu.php
+++ shared/menu.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" type="text/css" href="../assets/css/style_menu.css">
 <nav class="navbar navbar-expand-lg navbar-light bg-light" id="nav_principal">
-  <a class="navbar-brand" href="#"><?=$tituloPagina?></a>
+  <div style="width: 30%;"><a class="navbar-brand" href="#"><?=$tituloPagina?></a></div>
   <a class="navbar-brand" href="../carro/index.php"><img src="../assets/imagenes/carro_compras.svg" width="40" height="40" alt=""></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -13,8 +13,8 @@
   </nav>
   -->
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent" style="margin-left: 40%;">
-    <ul id="menu_opciones" class="navbar-nav mr-auto">
+  <div class="collapse navbar-collapse" id="navbarSupportedContent" style="margin-left: 30%;">
+    <ul class="navbar-nav mr-auto">
         <?php
         $menu = [
           'Home'       => '../home/index.php',
@@ -25,7 +25,11 @@
           //'Salir' => '../seguridad/login.php',
         ];
 
-            echo "<label id='nombre_usuario' style='margin-top: 3%;''>".$_SESSION['usuario']['nombre']."|</label>";
+            //echo "<label id='nombre_usuario' style='margin-top: 3%;''>".$_SESSION['usuario']['nombre']."|</label>";
+         echo "<li class='nav-item'>
+                    <strong><a class='nav-link' href='#'>" . $_SESSION['usuario']['nombre'] ."|</strong></a>
+                  </li>";
+
         foreach ($menu as $key => $value) {
             echo "<li class='nav-item'>
                     <a class='nav-link' href='$value'>$key</a>
